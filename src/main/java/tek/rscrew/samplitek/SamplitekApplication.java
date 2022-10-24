f051fd4be432029ae137738613f2669d4f1e6671
Aurase-xnx <aurasebox@gmail.com>
2022-10-24 14:53:18 +0200

    First Commit

---
+++ src/main/java/tek/rscrew/samplitek/SamplitekApplication.java
@@ -0,0 +1,13 @@
+package tek.rscrew.samplitek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamplitekApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SamplitekApplication.class, args);
+    }
+
+}
---
+++ src/test/java/tek/rscrew/samplitek/SamplitekApplicationTests.java
@@ -0,0 +1,13 @@
+package tek.rscrew.samplitek;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamplitekApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
