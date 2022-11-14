d718368a92bcd2fc7b7ca08928f053418f1a0d50
Aurase-xnx <aurasebox@gmail.com>
2022-11-14 09:49:57 +0100

    Test Model

--- src/main/java/tek/rscrew/samplitek/SamplitekController.java
+++ src/main/java/tek/rscrew/samplitek/SamplitekController.java
@@ -2,11 +2,14 @@ package tek.rscrew.samplitek;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
 
 @RestController
 public class SamplitekController {
     @GetMapping("/")
-    public String index(){
-        return "SMAPLETOUK";
+    public ModelAndView index(){
+        ModelAndView modelAndView = new ModelAndView();
+        modelAndView.setViewName("index.html");
+        return modelAndView;
     }
 }
