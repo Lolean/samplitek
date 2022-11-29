c11a734d01f9e54ceaed129d9f55527ef91890cd
Lolean <loloris2009@gmail.com>
2022-11-29 11:20:01 +0100

    rest_initialCommit

---
+++ src/test/java/tek/rscrew/samplitek/PostgresDockerTest.java
@@ -0,0 +1,13 @@
+package tek.rscrew.samplitek;
+
+
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@ContextConfiguration(initializers={UserRepositoryTCIntegrationTest.Initializer.class)
+public class PostgresDockerTest extends UserRepositoryCommonIntegrationTests {
+}
