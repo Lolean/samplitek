6550ac981cdd8b83989ea14428c52c6feb207cd9
Lolean <loloris2009@gmail.com>
2022-11-29 11:42:00 +0100

    project_structure

--- src/main/java/tek/rscrew/samplitek/adapter/in/SamplitekController.java
+++ src/main/java/tek/rscrew/samplitek/adapter/in/SamplitekController.java
@@ -1,4 +1,4 @@
-package tek.rscrew.samplitek;
+package tek.rscrew.samplitek.adapter.in;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -12,4 +12,9 @@ public class SamplitekController {
         modelAndView.setViewName("index.html");
         return modelAndView;
     }
+
+    
+
+
+
 }
